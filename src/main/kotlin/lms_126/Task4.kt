d3d1b945556e999ccfd7728317de1e588e184988
Linuslarsson3 <skinnbolinus@gmail.com>
2023-01-25 10:23:04 +0100

    Solution task 4
    If x is greater  than y print x if else prin y

--- src/main/kotlin/lms_126/Task4.kt
+++ src/main/kotlin/lms_126/Task4.kt
@@ -2,5 +2,8 @@ package lms_126
 
 fun task4(): String {
 
-    return TODO("Replace this 'TODO' with your solution")
+    return """
+       PRINT X if X is greater than Y
+       PRINT Y if Y is greater Than X 
+    """
 }
