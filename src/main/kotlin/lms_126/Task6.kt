d45435a5bad97ad6a8e2eba1f6f74c8174df3a9a
Linuslarsson3 <skinnbolinus@gmail.com>
2023-01-26 10:28:07 +0100

    task 6 solution
    I made sure that it output every number betwen 1 and 100

--- src/main/kotlin/lms_126/Task6.kt
+++ src/main/kotlin/lms_126/Task6.kt
@@ -2,5 +2,8 @@ package lms_126
 
 fun task6(): String {
 
-    return TODO("Replace this 'TODO' with your solution")
+    return """
+       OUTPUT every number between 1 and 100 
+       OUTPUT every number multiples of 5 between 1 and 100
+    """
 }
