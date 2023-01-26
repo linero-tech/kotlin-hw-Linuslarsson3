d26d456ca263168aad506c9507cdbdf12625dad6
Linuslarsson3 <skinnbolinus@gmail.com>
2023-01-26 10:06:14 +0100

    Solution task 5
    I set upp what x is and teling what hapends on a number

--- src/main/kotlin/lms_126/Task5.kt
+++ src/main/kotlin/lms_126/Task5.kt
@@ -2,5 +2,15 @@ package lms_126
 
 fun task5(): String {
 
-    return TODO("Replace this 'TODO' with your solution")
+    return """
+        X is a number between 1 and 8 
+        PRINT monday if x = 1
+        PRINT tuesday if x = 2
+        PRINT wednesday if x = 3
+        PRINT thursday if x = 4
+        PRINT friday if x = 5
+        PRINT saturday if x = 6
+        PRINT sunday if x = 7
+         PRINT monday if x = 8
+    """
 }
