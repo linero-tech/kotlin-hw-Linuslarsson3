a1c8bebefc59658e56613d189e99e41252bf2035
Linuslarsson3 <skinnbolinus@gmail.com>
2023-01-27 10:11:30 +0100

    task 11
    The convertion of celsius to farenhat is c * 9/5 +32

--- src/main/kotlin/lms_126/Task11.kt
+++ src/main/kotlin/lms_126/Task11.kt
@@ -2,5 +2,7 @@ package lms_126
 
 fun task11(): String {
 
-    return TODO("Replace this 'TODO' with your solution")
+    return """
+       PRINT 20C convert to F (20C * 9/5)+ 32 = 68F
+    """
 }
