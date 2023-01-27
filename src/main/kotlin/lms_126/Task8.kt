b0642134e9c0082bbdab060780089a0cd9cca198
Linuslarsson3 <skinnbolinus@gmail.com>
2023-01-27 09:51:30 +0100

    solution 8
    I uset when for the different viarbels

--- src/main/kotlin/lms_126/Task8.kt
+++ src/main/kotlin/lms_126/Task8.kt
@@ -2,5 +2,10 @@ package lms_126
 
 fun task8(): String {
 
-    return TODO("Replace this 'TODO' with your solution")
+    return """
+       PRINT Foo when x is a multiple of 3 
+       PRINT Bar when x is a multiple of 5
+       PRINT FooBAR when x is a multiple of 3 and 5 
+       PRINT Qix when x is not a multiple of 5 and 3 
+    """
 }
