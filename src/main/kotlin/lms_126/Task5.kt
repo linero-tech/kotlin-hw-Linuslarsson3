1d6339f2abbcf963fa354b9b94157ccb5906b45e
Linuslarsson3 <skinnbolinus@gmail.com>
2023-02-07 14:00:59 +0100

    Solution task 5
    Using if and else to solve the problem

--- src/main/kotlin/lms_126/Task5.kt
+++ src/main/kotlin/lms_126/Task5.kt
@@ -4,13 +4,13 @@ fun task5(): String {
 
     return """
         X is a number between 1 and 8 
-        PRINT monday if x = 1
-        PRINT tuesday if x = 2
-        PRINT wednesday if x = 3
-        PRINT thursday if x = 4
-        PRINT friday if x = 5
-        PRINT saturday if x = 6
-        PRINT sunday if x = 7
-         PRINT monday if x = 8
+        IF x = 1 PRINT monday
+        ELSE x = 2 PRINT tuseday
+        ELSE x = 3 PRINT wensday
+        ELSE x = 4 PRINT thursday
+        ELSE x = 5 PRINT friday
+        ELSE x = 6 PRINT saturday
+        ELSE x = 7 PRINT sunday
+        ELSE x = 8 PRINT sunday
     """
 }
