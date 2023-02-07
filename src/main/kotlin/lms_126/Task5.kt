742bef0caa79cc94dec5c0b0e7f8b50a1e9ad440
Linuslarsson3 <skinnbolinus@gmail.com>
2023-02-07 18:53:13 +0100

    5 and 8
    I Change how i write the ansers and corected a mistake

--- src/main/kotlin/lms_126/Task5.kt
+++ src/main/kotlin/lms_126/Task5.kt
@@ -4,13 +4,13 @@ fun task5(): String {
 
     return """
         X is a number between 1 and 8 
-        IF x = 1 PRINT monday
-        ELSE x = 2 PRINT tuseday
-        ELSE x = 3 PRINT wensday
-        ELSE x = 4 PRINT thursday
-        ELSE x = 5 PRINT friday
-        ELSE x = 6 PRINT saturday
-        ELSE x = 7 PRINT sunday
-        ELSE x = 8 PRINT sunday
+       IF x = 1 PRINT monday
+       IF ELSE x = 2 PRINT tuseday
+       IF ELSE x = 3 PRINT wensday
+       IF ELSE x = 4 PRINT thursday
+       IF ELSE x = 5 PRINT friday
+       IF ELSE x = 6 PRINT saturday
+       IF ELSE x = 7 PRINT sunday
+       IF ELSE x = 8 PRINT Eror message
     """
 }
--- src/main/kotlin/lms_126/Task8.kt
+++ src/main/kotlin/lms_126/Task8.kt
@@ -3,9 +3,9 @@ package lms_126
 fun task8(): String {
 
     return """
-       PRINT Foo when x is a multiple of 3 
-       PRINT Bar when x is a multiple of 5
-       PRINT FooBAR when x is a multiple of 3 and 5 
-       PRINT Qix when x is not a multiple of 5 and 3 
+      IF X = a multiple of 3 PRINT Foo
+      ELSE IF X = a multiple of 5 PRINT bar
+      ELSE IF X = a multiple of 3 and 5 PRINT Foobar 
+      ELSE IF X = a not a multiple of 3 or 5 PRINT Qix
     """
 }
