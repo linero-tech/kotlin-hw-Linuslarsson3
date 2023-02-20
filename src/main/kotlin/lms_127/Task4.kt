c21a43208cba81d38b2ae2c062bc83440874ea65
Linuslarsson3 <skinnbolinus@gmail.com>
2023-02-20 08:51:03 +0100

    solution 4
    I setup base * height / 2 and print the result

--- src/main/kotlin/lms_127/Task4.kt
+++ src/main/kotlin/lms_127/Task4.kt
@@ -1,6 +1,11 @@
 package lms_127
 
 fun task4(base: Double, height: Double): Double {
-
-    return TODO("Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword")
+    val result = base * height / 2
+    return result
+}
+fun main()
+{
+    val result = task4(base = 5.0, height = 10.0 )
+    println(result)
 }
