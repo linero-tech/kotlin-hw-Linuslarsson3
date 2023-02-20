19e5cf5f8400986cecdb8400d98f41082356b66b
Linuslarsson3 <skinnbolinus@gmail.com>
2023-02-20 08:35:20 +0100

    Task 2 solution
    I made the strings and used them in the sentence given in the qustion

--- src/main/kotlin/lms_127/Task2.kt
+++ src/main/kotlin/lms_127/Task2.kt
@@ -2,5 +2,14 @@ package lms_127
 
 fun task2() {
 
+val name = "Linus"
+val country = "Sweden"
+val month = "May"
 
+println("Hello $name you are from $country and you were born in $month")
+    return
+}
+fun main()
+{
+  task2()
 }
