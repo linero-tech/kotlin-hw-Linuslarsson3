ab2ecc504bf8870ae8bf5f5a9d11ed09f00bf6ec
Linuslarsson3 <skinnbolinus@gmail.com>
2023-03-01 15:58:12 +0100

    Task 1
    first setup so a and b interakt ecother and then make sure so if a is more and equle to b result is zero

--- src/main/kotlin/lms_129/Task1.kt
+++ src/main/kotlin/lms_129/Task1.kt
@@ -2,5 +2,15 @@ package lms_129
 
 fun task1(a: Int, b: Int): Int {
 
-    return TODO("Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword")
+    var result = 0
+    for (i in a..b){
+        result = i
+        if (a >= b){
+            result = 0
+        }
+    }
+    return result
+}
+fun main (){
+    println(task1(a = 1, b = 5))
 }
