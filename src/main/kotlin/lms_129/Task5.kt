ea45034e7bf9b0d52b8308774f9b6075269f0d8a
Linuslarsson3 <skinnbolinus@gmail.com>
2023-03-03 09:30:16 +0100

    Takk 5 solution
    First i made the varibel sentence put it in a for lop then i printed it

--- src/main/kotlin/lms_129/Task5.kt
+++ src/main/kotlin/lms_129/Task5.kt
@@ -2,5 +2,15 @@ package lms_129
 
 fun task5(sentence: String): Int {
 
-    return TODO("Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword")
+    val sentence = sentence.length
+    for (i in sentence until sentence){
+        if(sentence>= 0) {
+            sentence
+        }else
+            println("no sentence")
+    }
+    return sentence
 }
+  fun main(){
+      println(task5(sentence = "Bulbasur"))
+  }
