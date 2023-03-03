9c2cae63681caaa1f973670ae49a5d62f5a803f2
Linuslarsson3 <skinnbolinus@gmail.com>
2023-03-03 08:54:39 +0100

    takk 4 Solution
    Print all number then make so they are ale devicebel by 9

--- src/main/kotlin/lms_129/Task2.kt
+++ src/main/kotlin/lms_129/Task2.kt
@@ -1,6 +1,7 @@
 package lms_129
 
 fun task2(number: Int): Boolean {
+    var result = 1
 
     return TODO("Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword")
 }
--- src/main/kotlin/lms_129/Task4.kt
+++ src/main/kotlin/lms_129/Task4.kt
@@ -2,5 +2,15 @@ package lms_129
 
 fun task4(): Int {
 
-    return TODO("Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword")
+     var result = 1
+    for(number in 1..1000){
+        if(number % 9 ==0){
+          result = number
+            println(number)
+        }
+    }
+return result
+}
+fun main (){
+println(task4())
 }
