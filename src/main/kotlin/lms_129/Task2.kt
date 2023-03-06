fc83a460c6f834296c81ed9748a647d94224c4aa
Linuslarsson3 <skinnbolinus@gmail.com>
2023-03-06 10:35:32 +0100

    Solution 7
    Setup for loop a and b if a and b == 0 then = 1 else result = a*b

--- src/main/kotlin/lms_129/Task2.kt
+++ src/main/kotlin/lms_129/Task2.kt
@@ -1,7 +1,6 @@
 package lms_129
 
 fun task2(number: Int): Boolean {
-    var result = 1
 
-    return TODO("Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword")
+    return true
 }
--- src/main/kotlin/lms_129/Task7.kt
+++ src/main/kotlin/lms_129/Task7.kt
@@ -2,5 +2,17 @@ package lms_129
 
 fun task7(a: Int, b: Int): Int {
 
-    return TODO("Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword")
+   var result = 1
+
+    for(number in a..b){
+        if( a + b == 0){
+ result = 1
+        } else {
+            result = a*b
+        }
+    }
+    return result
+}
+fun main (){
+    println(task7(a = 4, b = 6))
 }
