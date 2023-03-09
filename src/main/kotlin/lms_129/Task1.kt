a6cd3c50079ecb612bb34ae591c693d0a6e568b3
Linuslarsson3 <skinnbolinus@gmail.com>
2023-03-09 09:06:37 +0100

    solution task 1
    .

--- src/main/kotlin/lms_129/Task1.kt
+++ src/main/kotlin/lms_129/Task1.kt
@@ -4,13 +4,16 @@ fun task1(a: Int, b: Int): Int {
 
     var result = 0
     for (i in a..b){
-        result = i
+
         if (a >= b){
             result = 0
+        }else{
+            result += i
         }
+
     }
     return result
 }
 fun main (){
-    println(task1(a = 1, b = 5))
+    println(task1(a = 3, b = 3))
 }
